5cc76eced02fd77943187d4533524b9561af5fa6
willasur <87398003+willasur@users.noreply.github.com>
2021-10-27 09:47:27 -0700

    Add movieDetails class

---
+++ MoviesDB/Models/movieDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesDB.Models
+{
+    public class movieDetails
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+    }
+}
