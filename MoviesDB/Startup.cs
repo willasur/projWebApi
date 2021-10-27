b53cfb5fecbca6433225ecb7c4f7ff8ba8a710b0
willasur <87398003+willasur@users.noreply.github.com>
2021-10-27 09:55:27 -0700

    Remove swagger calls

--- MoviesDB/Startup.cs
+++ MoviesDB/Startup.cs
@@ -28,10 +28,10 @@ namespace MoviesDB
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesDB", Version = "v1" });
-            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesDB", Version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +40,8 @@ namespace MoviesDB
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesDB v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesDB v1"));
             }
 
             app.UseHttpsRedirection();
